5c15384116672b40853ed2c05477da5ada8bb279
Israel Saeta Pérez <dukebody@gmail.com>
2009-11-21 21:56:28 +0100

    Print index closes and acceleration calcularions to spread.txt.

--- compare.py
+++ compare.py
@@ -12,4 +12,12 @@ index_closes = get_closes(INDEX)
 closes_list = [closes[ticker] for ticker in tickers]
 mean_point_acceleration = get_mean_point_accelerations(closes_list, index_closes)
 
-print mean_point_acceleration
+
+columns = [['index'] + stringify(index_closes)[2:], ['accel'] + stringify(mean_point_acceleration)]
+rows = zip(*columns)
+out = open('spread.txt', 'w')
+for row in rows:
+    out.write(' '.join(row))
+    out.write('\n')
+out.close()
+
