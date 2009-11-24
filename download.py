ed7f8ff7070c2bdc19bb9af4bb8816791f9a17d7
Israel Saeta Pérez <dukebody@gmail.com>
2009-11-24 11:56:59 +0100

    Create method to download all historical data to avoid downloading it dozens of times.

---
+++ download.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import utils
+import urllib2
+
+sap_tickers = utils.get_tickers()
+sap_tickers += [utils.INDEX]
+
+for ticker in sap_tickers:
+    try:
+        utils.download_historical_data(ticker)
+    except urllib2.HTTPError:
+        print 'data not found for ticker: %s' % ticker
