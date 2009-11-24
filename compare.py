e4bfe684707eeafef2bf45874c734d61fb6d3e6f
Israel Saeta Pérez <dukebody@gmail.com>
2009-11-24 12:45:34 +0100

    Get all tickers from the index and use means instead of the index for comparison.

--- compare.py
+++ compare.py
@@ -4,7 +4,7 @@ from utils import *
 import urllib2
 
 
-sap_tickers = get_tickers()[80:90]
+sap_tickers = get_tickers()
 
 closes = get_closes_from_tickerslist(sap_tickers)
 index_closes = get_closes(INDEX)
@@ -12,10 +12,11 @@ index_closes = get_closes(INDEX)
 valid_tickers = closes.keys()
 
 closes_list = [closes[ticker] for ticker in valid_tickers]
-mean_point_acceleration = get_mean_point_accelerations(closes_list, index_closes)
+weekly_mean = point_mean(closes_list)
+mean_point_acceleration = get_mean_point_accelerations(closes_list, weekly_mean)
 
 
-columns = [['index'] + stringify(index_closes)[2:], ['accel'] + stringify(mean_point_acceleration)]
+columns = [['index'] + stringify(weekly_mean)[2:], ['accel'] + stringify(mean_point_acceleration)]
 rows = zip(*columns)
 out = open('spread.txt', 'w')
 for row in rows:
