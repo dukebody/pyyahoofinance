3bcd99ad5bcd911fb2361bd130c5347c2b0a5d05
Israel Saeta Pérez <dukebody@gmail.com>
2009-12-01 13:29:19 +0100

    Use diffs instead of mean (I don't really know what I did here).

--- compare.py
+++ compare.py
@@ -13,10 +13,12 @@ valid_tickers = closes.keys()
 
 closes_list = [closes[ticker] for ticker in valid_tickers]
 weekly_mean = point_mean(closes_list)
+weekly_diffs = get_diffs(weekly_mean)
 mean_point_acceleration = get_mean_point_accelerations(closes_list, weekly_mean)
 
 
-columns = [['index'] + stringify(weekly_mean)[2:], ['accel'] + stringify(mean_point_acceleration)]
+columns = [['index'] + stringify(weekly_diffs)[1:], ['accel'] + stringify(mean_point_acceleration)]
+assert len(columns[0]) == len(columns[1])
 rows = zip(*columns)
 out = open('spread.txt', 'w')
 for row in rows:
