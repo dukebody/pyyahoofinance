b4456a69e671a0f0d654ade209204565d993f683
Israel Saeta Pérez <dukebody@gmail.com>
2009-12-01 15:53:59 +0100

    Add dates and column labels.

--- spread.py
+++ spread.py
@@ -6,28 +6,34 @@ from stockmarket import Stock, Market
 sap_tickers = utils.get_tickers() # Standard & Poor's tickers
 closes = utils.get_closes_from_tickerslist(sap_tickers)
 valid_tickers = closes.keys()
+dates = utils.get_dates(valid_tickers[0])
 stocks = [Stock(ticker, closes[ticker]) for ticker in valid_tickers]
 market = Market(stocks)
 
 spread = []
 market_performance = []
+dates_shown = []
 STEP = 30
 
 for i in range(STEP, len(stocks[0].values)-STEP, STEP):
-    start = i
-    end = i+STEP
+    previous = i - STEP
+    now = i
+    next = i + STEP
     strong_stocks = [s for s in stocks 
-                     if s.getOffensive(start, end) >= 100 and s.getDefensive(start, end) <= 100]
+                     if s.getOffensive(previous, now) >= 100 and s.getDefensive(previous, now) <= 100]
     weak_stocks = [s for s in stocks 
-                   if s.getOffensive(start, end) <= 100 and s.getDefensive(start, end) >= 100]
+                   if s.getOffensive(previous, now) <= 100 and s.getDefensive(previous, now) >= 100]
  
-    mean_strong_performance = utils.mean([s.getPerformance(start+STEP, end+STEP) for s in strong_stocks])
-    mean_weak_performance = utils.mean([s.getPerformance(start+STEP, end+STEP) for s in weak_stocks])
+    mean_strong_performance = utils.mean([s.getPerformance(now, next) for s in strong_stocks])
+    mean_weak_performance = utils.mean([s.getPerformance(now, next) for s in weak_stocks])
     spread.append(mean_strong_performance - mean_weak_performance)
-    market_performance.append(market.getPerformance(start, end))
+    market_performance.append(market.getPerformance(previous, now))
+    dates_shown.append(dates[now])
 
 
-columns = (utils.stringify(spread), utils.stringify(market_performance))
+columns = (['dates'] + dates_shown, 
+           ['spread'] + utils.stringify(spread), 
+           ['market'] + utils.stringify(market_performance))
 rows = zip(*columns)
 out = open('spread.txt', 'w')
 for row in rows:
